4857ad63350e74ffa5ed3dd1b71f5f0bade8becc
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2020-08-27 06:16:55 +0800

    55

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,39 +1,14 @@
 public class MainTest
 {
-    static abstract class Human
-    {
-    }
-    static class Man extends Human
-    {
-    }
-    static class Woman extends Human
-    {
-    }
-
-    public void sayHello(Human guy)
-    {
-        System.out.println("Human");
-    }
-    public void sayHello(Man guy)
-    {
-        System.out.println("Man");
-    }
-    public void sayHello(Woman guy)
-    {
-        System.out.println("Women");
-    }
-
     public static void main(String[] args)
     {
-        Human man = new Man();
-        Human women = new Woman();
-        MainTest mainTest = new MainTest();
-        mainTest.sayHello((Man)man);
-        mainTest.sayHello(women);
-        /*
-        result:
-            Human
-            Human
-         */
+        if(true)
+        {
+            System.out.println("1");
+        }
+        else
+        {
+            System.out.println("2");
+        }
     }
 }
