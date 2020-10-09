3f392874a4a3a839b1bd71f549f4c59db1ba5f74
tongji4m3 <1254931237@qq.com>
2020-10-09 15:28:03 +0800

    Create Demo226.java

---
+++ src/hot/Demo226.java
@@ -0,0 +1,15 @@
+package hot;
+
+import utils.TreeNode;
+
+public class Demo226
+{
+    public TreeNode invertTree(TreeNode root)
+    {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
