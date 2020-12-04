6872b3e4a6aa61b467d37a84211f7b36f65f02a1
tongji4m3 <1254931237@qq.com>
2020-12-04 09:30:52 +0800

    Demo279

--- src/hot/Demo240.java
+++ src/hot/Demo240.java
@@ -1,12 +1,12 @@
 package hot;
 
+
 public class Demo240 {
     public boolean searchMatrix(int[][] matrix, int target) {
         //从右上开始寻找,左边都是小于他的,下边都是大于他的
         if (matrix == null || matrix.length == 0) {
             return false;
         }
-
         int m = matrix.length, n = matrix[0].length;
         int i = 0, j = n - 1;
         while (i < m && j >= 0) {
