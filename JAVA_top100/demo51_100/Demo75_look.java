6fad3139a2b9f5bd66bed906866770c1f13118ad
tongji4m3 <1254931237@qq.com>
2020-12-05 11:15:48 +0800

    Demo287,297

--- JAVA_top100/demo51_100/Demo75_look.java
+++ JAVA_top100/demo51_100/Demo75_look.java
@@ -7,18 +7,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 //先序遍历
-public class Demo75_look
-{
-    public String serialize(TreeNode root)
-    {
-        StringBuilder result=recursive(root,new StringBuilder());
+public class Demo75_look {
+    public String serialize(TreeNode root) {
+        StringBuilder result = recursive(root, new StringBuilder());
         return result.toString();
     }
 
-    private StringBuilder recursive(TreeNode root, StringBuilder stringBuilder)
-    {
-        if(root==null)
-        {
+    private StringBuilder recursive(TreeNode root, StringBuilder stringBuilder) {
+        if (root == null) {
             stringBuilder.append("null ");
             return stringBuilder;
         }
@@ -29,25 +25,21 @@ public class Demo75_look
         return stringBuilder;
     }
 
-    public TreeNode deserialize(String data)
-    {
+    public TreeNode deserialize(String data) {
         String[] temp = data.split(" ");
         List<String> list = new LinkedList<>(Arrays.asList(temp));
         return recursive1(list);
     }
 
-    private TreeNode recursive1(List<String> list)
-    {
+    private TreeNode recursive1(List<String> list) {
         //不管如何,list起码有一个null元素
         TreeNode root;
 
-        if(list.get(0).equals("null"))
-        {
+        if (list.get(0).equals("null")) {
             list.remove(0);
             return null;
         }
-        else
-        {
+        else {
             root = new TreeNode(Integer.valueOf(list.get(0)));
             list.remove(0);
             root.left = recursive1(list);
@@ -57,8 +49,7 @@ public class Demo75_look
         return root;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
