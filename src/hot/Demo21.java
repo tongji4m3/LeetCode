ff41725a188825b42d35ecc6b7f78f54b038a983
tongji4m3 <1254931237@qq.com>
2020-12-07 17:06:36 +0800

    Update Demo21.java

--- src/hot/Demo21.java
+++ src/hot/Demo21.java
@@ -2,65 +2,45 @@ package hot;
 
 import utils.ListNode;
 
-public class Demo21
-{
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
-    {
+public class Demo21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
 
         ListNode result = null;
 
-        if (l1.val < l2.val)
-        {
+        if (l1.val < l2.val) {
             result = new ListNode(l1.val);
             result.next = mergeTwoLists(l1.next, l2);
         }
-        else
-        {
+        else {
             result = new ListNode(l2.val);
             result.next = mergeTwoLists(l1, l2.next);
         }
         return result;
     }
 
-    public ListNode mergeTwoLists1(ListNode l1, ListNode l2)
-    {
-        ListNode pre = new ListNode(-1), curr = pre;
-        while (l1 != null && l2 != null)
-        {
-            if (l1.val < l2.val)
-            {
-                ListNode temp = new ListNode(l1.val);
-                curr.next = temp;
-                curr = temp;
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = new ListNode(l1.val);
                 l1 = l1.next;
+                cur = cur.next;
             }
-            else
-            {
-                ListNode temp = new ListNode(l2.val);
-                curr.next = temp;
-                curr = temp;
+            else {
+                cur.next = new ListNode(l2.val);
                 l2 = l2.next;
+                cur = cur.next;
             }
         }
-
-        while (l1 != null)
-        {
-            ListNode temp = new ListNode(l1.val);
-            curr.next = temp;
-            curr = temp;
-            l1 = l1.next;
+        if (l1 == null) {
+            cur.next = l2;
         }
-
-        while (l2 != null)
-        {
-            ListNode temp = new ListNode(l2.val);
-            curr.next = temp;
-            curr = temp;
-            l2 = l2.next;
+        else {
+            cur.next = l1;
         }
-
-        return pre.next;
+        return head.next;
     }
 }
