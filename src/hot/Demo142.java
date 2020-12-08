3f9ead140cfc795ac7f407b45e8858530bedc029
tongji4m3 <1254931237@qq.com>
2020-12-08 22:02:41 +0800

    链表题

--- src/hot/Demo142.java
+++ src/hot/Demo142.java
@@ -2,27 +2,21 @@ package hot;
 
 import utils.ListNode;
 
-import java.util.List;
-
-public class Demo142
-{
-    public ListNode detectCycle(ListNode head)
-    {
-        ListNode fast = head, slow = head;
-        while (true)
-        {
-            if(fast == null || fast.next == null) return null;
+public class Demo142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
             fast = fast.next.next;
-            slow = slow.next;
-            if (slow == fast) break;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
         }
-        //必然有循环
-        fast = head;
-        while (true)
-        {
-            if(slow==fast) return fast;
-            fast = fast.next;
-            slow = slow.next;
-        }
+        return null;
     }
 }
