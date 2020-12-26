16dc077b8af6ebbeb419bffa269368b9d69e5509
tongji4m3 <1254931237@qq.com>
2020-12-26 16:08:27 +0800

    剑指Offer刷完第一遍了

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,17 @@
 public class MainTest {
     public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c==d);//true
+        System.out.println(e==f);//false
+        System.out.println(c==(a+b));//true
+        System.out.println(c.equals(a+b));//true
+        System.out.println(g==(a+b));//true
+        System.out.println(g.equals(a+b));//false
     }
 }
