b9b7ca4c0d050f88fcd0bb554c621126f5689d11
tongji4m3 <1254931237@qq.com>
2021-02-05 20:43:03 +0800

    刷题

---
+++ src/niuke/JZ58.java
@@ -0,0 +1,17 @@
+package niuke;
+
+import utils.TreeNode;
+
+public class JZ58 {
+    boolean isSymmetrical(TreeNode root) {
+        if (root == null) return true;
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+}
